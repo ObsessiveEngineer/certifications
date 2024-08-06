02a8241f686a1404fa37041b7767389aeae97ebb
Ferhat Yenilmez <ynlmzferhat@gmail.com>
2024-08-06 15:11:09 +0300

    feat(100-days-of-code-the-complete-python-pro-bootcamp/): Add a day-1 project called 'Band Name Generator' and 3 exercises/

---
+++ 100-days-of-code-the-complete-python-pro-bootcamp/day-1/band_name_generator.py
@@ -0,0 +1,7 @@
+################ Project: Band Name Generator ################
+
+print("Welcome to the Band Name Generator!")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pets name?\n")
+band_name = city + " " + pet
+print("Your band name could be " + band_name)
